beaeec2ac03bd3bb33b77968b095db638559a1d9
m-nulph <007spy@gmail.com>
2015-10-28 19:35:38 -0400

    temp commit

--- controller/app.py
+++ controller/app.py
@@ -2,23 +2,38 @@ import simplejson as json
 import datetime
 from contextlib import contextmanager
 import getpass
+import cv2
+import sys
+sys.path.insert(0, '/home/matt/Downloads/wiibalance/cwiid/python/build/lib.linux-x86_64-2.7/')
+import cwiid
+import time
+import wiiweight
 
 from flask import Flask, request
+from item import Item
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy.ext.declarative import declarative_base
 
 app = Flask(__name__)
 
 psql_engine = create_engine('postgresql://' + getpass.getuser() +
                             ':15291@localhost/main')
-session_factory = sessionmaker(bind=psql_engine)
-Session = scoped_session(session_factory)
+Session = sessionmaker(bind=psql_engine)
+#Session = scoped_session(session_factory)
 
-Base = declarative_base()
+import item
 
-Base.metadata.create_all(psql_engine)
+item.Base.metadata.create_all(psql_engine)
+
+#Builtin webcam for laptop is 0, usb cam is 1
+camera_port = 0
+#image ID
+imgID = -1
+#Initialize camera
+webcam = cv2.VideoCapture(camera_port)
+
+
 
 @contextmanager
 def dbSession():
@@ -34,25 +49,27 @@ def hello():
 
 @app.route("/add", methods=['POST'])
 def add_item():
-    with dbSession() as dbSession:
-        label = request.form['label']
-        amount = request.form['amount']
-        unit = request.form['unit']
-        year = int(request.form['year'])
-        month = int(request.form['month'])
-        day = int(request.form['day'])
-        picture_id = int(request.form['picture_id'])
-        new_item = Item(label, amount, unit,
+    dbSession = Session()
+#    with dbSession() as dbSession:
+    label = request.form['label']
+    amount = request.form['amount']
+    unit = request.form['unit']
+    year = int(request.form['year'])
+    month = int(request.form['month'])
+    day = int(request.form['day'])
+    picture_id = int(request.form['picture_id'])
+    new_item = Item(label, amount, unit,
                         datetime.date(year, month, day), picture_id)
-        dbSession.add(new_item)
-        dbSession.commit()
+    dbSession.add(new_item)
+    dbSession.commit()
     return json.dumps({ 'status' : 'success' })
 
 @app.route("/items", methods=['GET'])
 def list_items():
-    with dbSession() as dbSession:
-        items = dbSession.query(Item).all()
-        ret = [i.as_dict() for i in items]
+#    with dbSession() as dbSession:
+    dbSession = Session()
+    items = dbSession.query(Item).all()
+    ret = [i.as_dict() for i in items]
     return json.dumps(ret)
 
 @app.route("/items/<int:id>", methods=['GET'])
@@ -64,15 +81,48 @@ def weight():
     # TODO: Finish this
     pass
 
+def get_image():
+    #Gets image from webcam
+    retval, img = webcam.read()
+    return img
+
 @app.route("/take_picture", methods=['POST'])
 def take_picture():
     # TODO: Finish this
     # Should return a picture ID
+    global webcam
+    global imgID
+
+    #throw away frames for camera adjustment
+    ramp_frames = 30
+
+    #Increment imgID counter
+    imgID += 1
+
+    for i in xrange(ramp_frames):
+        temp = get_image()
+
+    print("Taking Image...")
+
+    captureimg = get_image()
+
+    #File to write to
+    file = "../../images/img" + str(imgID) + ".png"
+
+    cv2.imwrite(file, captureimg)
+
+    # Release webcam for additional pictures
+    del (webcam)
+
+    return imgID
     pass
 
 @app.route("/pictures/<int:picture_id>/", methods=['GET'])
 def get_picture(picture_id):
     pass
 
+take_picture()
+#wiiweight.get_weight()
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
+#    app.run(debug=True)
--- controller/item.py
+++ controller/item.py
@@ -1,5 +1,9 @@
-from sqlalchemy import Column, Integer, String, DateTime, Enum
-from app import Base
+from sqlalchemy import Column, Integer, String, DateTime, Enum, Float
+
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
 
 class Item(Base):
     __tablename__ = 'Item'
@@ -7,23 +11,23 @@ class Item(Base):
     id = Column(Integer, primary_key=True)
     label = Column(String)
     amount = Column(Float)
-    unit = Column(Enum("Count", "Kilogram", "Liters"))
+    unit = Column(Enum("Count", "Kilogram", "Liters", name="unit"))
     expiration = Column(DateTime)
     picture_id = Column(Integer)
 
-    def __init__(label, amount, unit, expiration, picture_id):
+    def __init__(self, label, amount, unit, expiration, picture_id):
         self.label = label
         self.amount = amount
         self.unit = unit
         self.expiration = expiration
         self.picture_id = picture_id
 
-    def as_dict():
-        return { 'label' : label,
-                 'amount' : amount,
-                 'unit' : unit,
+    def as_dict(self):
+        return { 'label' : self.label,
+                 'amount' : self.amount,
+                 'unit' : self.unit,
                  'expiration' :
-                  { 'year' : expiration.year(),
-                    'month' : expiration.month(),
-                    'day' : expiration.day(), }
+                  { 'year' : self.expiration.year,
+                    'month' : self.expiration.month,
+                    'day' : self.expiration.day, }
                }
