6d3681bd324087272ec9babc5c4f9afcc51cd7c0
m-nulph <007spy@gmail.com>
2015-11-30 00:13:19 -0500

    Recipe/item lookup functions

---
+++ controller/userecipe.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+import json, requests
+import sys
+import Recipe
+
+def main():
+    
+    #request = Recipe.Recipe()
+    #RecipeData = request.gen_recipe_request(530115)
+    #print request.gen_recipe_request(RecipeData)
+
+    #request2 = Recipe.Recipe()
+    #AnyKeyword = "milk peaches sugar"
+    #RecipeData2 = request2.gen_search_any_request(AnyKeyword.replace(" ", "%20"))
+    #print request2.gen_recipe_request(RecipeData2)
+
+    request3 = Recipe.Recipe()
+    TitleKeyword = "Peach Cobbler"
+    RecipeData3 = request3.gen_search_title_request(TitleKeyword.replace(" ", "%20"))
+    print request3.gen_recipe_request(RecipeData3)
+
+    
+
+#    for ingredients in RecipeData['Ingredients']:
+#        print ingredients['Name']
+    
+
+#    print data
+
+if __name__ == "__main__":
+    sys.exit(main())
